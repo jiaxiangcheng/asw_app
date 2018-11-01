0e8d099facef7ec439299c37b6e644929df83733
fabio <fabio@asw.upc.edu>
2018-11-01 23:41:12 +0100

    FIXED: last commit is broken

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,2 +1,15 @@
 module SubmissionsHelper
+  # makes string with seconds/minutes/hours/days indication
+  def humanize_time(time)
+    case time # since submission
+    when time < 60
+      return time.to_s + " seconds ago"
+    when time < 60*60
+      return (time/60).round.to_s + " minutes ago"
+    when time < 60*60*24
+      return (time/(60*60)).round.to_s + " hours ago"
+    else
+      return (time/(60*60*24)).round.to_s + " days ago"
+    end
+  end
 end
