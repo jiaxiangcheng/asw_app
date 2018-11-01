4d678338a9367c1f50ff36850ffd2bc4ddfa541a
fabio <fabio@asw.upc.edu>
2018-11-01 23:41:12 +0100

    FIXED: last commit is broken

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'submissions#index'
 
+  get 'news' => 'submissions#index', as: :news_menu
   get 'newest' => 'newest#index'
   get 'ask' => 'submissions#ask'
 end
