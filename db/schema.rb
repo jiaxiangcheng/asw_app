bae153182772c1265a2a8bc1c015e7c2e787d579
fabio <fabio@asw.upc.edu>
2018-11-08 19:37:26 +0100

    submission.points removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181108121758) do
+ActiveRecord::Schema.define(version: 20181108171854) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "submission_id"
@@ -28,9 +28,15 @@ ActiveRecord::Schema.define(version: 20181108121758) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "points"
     t.integer "num_comments"
     t.integer "user_id"
+    t.integer "cached_votes_total", default: 0
+    t.integer "cached_votes_score", default: 0
+    t.integer "cached_votes_up", default: 0
+    t.integer "cached_votes_down", default: 0
+    t.integer "cached_weighted_score", default: 0
+    t.integer "cached_weighted_total", default: 0
+    t.float "cached_weighted_average", default: 0.0
     t.index ["user_id"], name: "index_submissions_on_user_id"
   end
 
