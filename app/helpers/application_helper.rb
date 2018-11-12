cac2fc11b0069d8754dbb8b95fc4b8b0d54bebf5
fabio <fabio@asw.upc.edu>
2018-11-12 04:52:11 +0100

    goto_url to return after vote and delete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def goto_url(anchor_id = '')
+    if anchor_id == ''
+      return request.original_url
+    else
+      return request.original_url + "#" + anchor_id.to_s
+    end
+  end
 end
