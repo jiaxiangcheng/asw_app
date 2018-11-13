1584be3025e413cf4ed590a2fda8b76aa7b6c6a0
fabio <fabio@asw.upc.edu>
2018-11-13 12:51:22 +0100

    migration correction for heroku on teacher instructions

--- db/migrate/20181111212630_reply_parent_polymorphy.rb
+++ db/migrate/20181111212630_reply_parent_polymorphy.rb
@@ -1,7 +1,7 @@
 class ReplyParentPolymorphy < ActiveRecord::Migration[5.1]
   def change
     change_table :comments do |t|
-      t.remove :parent_id, index: true, foreign_key: { to_table: :comments }
+      t.remove :parent_id
       t.references :parent, polymorphic: true, index: true
     end
   end
