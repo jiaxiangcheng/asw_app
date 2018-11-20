e149ef9e4a16cbb2395372ed67abaed841c5ed2f
fabio <fabio@asw.upc.edu>
2018-11-21 00:47:59 +0100

    swagger works offline; click the  button in hackernews

--- config/routes.rb
+++ config/routes.rb
@@ -33,4 +33,6 @@ Rails.application.routes.draw do
   get 'users/:uid' => 'users#index'
   get 'logout', to: 'sessions#destroy', as: :logout
   get '/submissions/:submission_id/comments/:id/showreply', to: 'comments#showreply'
+
+  get '/api', :to => redirect('/swagger-editor/index.html?url=/api-docs.yaml'), as: :swagger_editor
 end
