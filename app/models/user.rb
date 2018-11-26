e298e234e7893c589a0b1292c514a7ec03e96f06
fabio <fabio@asw.upc.edu>
2018-11-26 18:00:53 +0100

    tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,21 @@
 class User < ApplicationRecord
-  has_many :submissions
-  has_many :comments
+  has_many :submissions, dependent: :destroy
+  has_many :comments, dependent: :destroy
   acts_as_voter
-    def self.from_omniauth(auth)
-        where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-          user.provider = auth.provider
-          user.uid = auth.uid
-          user.name = auth.info.name
-          user.email = auth.info.email
-          user.token = auth.credentials.token
-          user.save!
-        end
+  validates :provider, presence: true
+  validates :uid, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :token, presence: true
+  
+  def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.name
+        user.email = auth.info.email
+        user.token = auth.credentials.token
+        user.save!
       end
+    end
 end
