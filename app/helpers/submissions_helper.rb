db7c304482ebb5867b361ff5596c154d4d67f40a
fabio <fabio@asw.upc.edu>
2018-11-28 03:22:01 +0100

    refactored is_url? and is_ask? helper methods

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -3,16 +3,6 @@ module SubmissionsHelper
   # def humanize_time(time)
   # end
 
-  # return true if is ask
-  def is_ask?(submission)
-    return submission.url == ''
-  end
-
-  # return true if is url
-  def is_url?(submission)
-    return submission.text == ''
-  end
-
   # extracts host from uri
   def shorten_url(url)
     return URI.parse(url).host
