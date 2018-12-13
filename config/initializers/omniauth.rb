2cce7d1bc5c27f3e27e8b2aab8a97042df3c5cc5
fabio <fabio@asw.upc.edu>
2018-12-13 18:47:47 +0100

    added secrets.yaml to .gitignore

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, Rails.application.secrets.google_oauth2_part1, Rails.application.secrets.Rails.application.secrets.google_oauth2_part2
+    provider :google_oauth2, Rails.application.secrets.google_oauth2_part1, Rails.application.secrets.google_oauth2_part2
 end
