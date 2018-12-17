2a5903d8c482797429314fb44184724b85d60bd9
fabio <fabio@asw.upc.edu>
2018-12-17 23:25:49 +0100

    bugfix: tokens deleted on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,13 @@ class SessionsController < ApplicationController
     login_response = request.env["omniauth.auth"]
     @auth = login_response['credentials']
     user = User.from_omniauth(login_response)
-    Token.create(
+    token = Token.create(
       access_token: @auth['token'],
       refresh_token: @auth['refresh_token'],
       expires_at: Time.at(@auth['expires_at']).to_datetime,
       user: user
-    ) # create also saves
+    )
+    token.save
     session[:user_id] = user.id
     redirect_to root_path
   end
@@ -16,7 +17,6 @@ class SessionsController < ApplicationController
   def destroy
     if session[:user_id] && User.exists?(session[:user_id])
       user = User.find(session[:user_id])
-      user.token.destroy
     end
     session[:user_id] = nil
     redirect_to root_path
