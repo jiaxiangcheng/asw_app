51f0c01150bc993ec095f7af3564aaafec88d73e
fabio <fabio@asw.upc.edu>
2018-12-18 16:35:35 +0100

    bugfix token username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,10 @@ class ApplicationController < ActionController::Base
     if session.key?(:user_id) && User.exists?(session[:user_id])
       # logged in
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    elsif request.headers.key?(:Token) && Token.exists?(access_token: request.headers[:Token])
-      # API call with valid token
-      token = Token.find_by(access_token: request.headers[:Token])
-      @current_user = token.user
+    # elsif request.headers.key?(:Token) && Token.exists?(access_token: request.headers[:Token])
+    #   # API call with valid token
+    #   token = Token.find_by(access_token: request.headers[:Token])
+    #   @current_user = token.user
     elsif request.headers.key?(:Token) && User.exists?(name: request.headers[:Token])
       @current_user = User.find_by(name: request.headers[:Token])
     else
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
     login_response = request.env["omniauth.auth"]
     @auth = login_response['credentials']
     user = User.from_omniauth(login_response)
-    token = Token.create(
-      access_token: @auth['token'],
-      refresh_token: @auth['refresh_token'],
-      expires_at: Time.at(@auth['expires_at']).to_datetime,
-      user: user
-    )
-    token.save
+    # token = Token.create(
+    #   access_token: @auth['token'],
+    #   refresh_token: @auth['refresh_token'],
+    #   expires_at: Time.at(@auth['expires_at']).to_datetime,
+    #   user: user
+    # )
+    # token.save
     session[:user_id] = user.id
     redirect_to root_path
   end
